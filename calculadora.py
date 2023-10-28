5749dc36c6f8bfbec05e71f98bfe20e84f8b7190
GuilhermeMFranco <148026827+GuilhermeMFranco@users.noreply.github.com>
2023-10-27 23:15:46 -0300

    Update calculadora.py
    Calculadora em Python. Calculadora com calculos simples.

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,8 @@
 import os
 
-primeiro_valor = float(input(f"Informe um valor para ser calculado: {os.linesep} [1] [2] [3] [/] {os.linesep} [4] [5] [6] [x] {os.linesep} [7] [8] [9] [-] {os.linesep} [#] [0] [*] [+]{os.linesep}"))
+primeiro_valor = float(input(f"Informe o primeir valor para ser calculado: {os.linesep} [1] [2] [3] [/] {os.linesep} [4] [5] [6] [x] {os.linesep} [7] [8] [9] [-] {os.linesep} [#] [0] [*] [+]{os.linesep}"))
 operacao = input("Escolha a operação desejada (+, -, *, /):")
-segundo_valor = float(input("Escolha outro número: "))
+segundo_valor = float(input("Informe o segundo valor: "))
 
 if operacao == "+":
     resultado = primeiro_valor + segundo_valor
