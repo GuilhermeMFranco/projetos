6130a8ec93ef9c34d27e209898a88ad1efda28c4
GuilhermeMFranco <148026827+GuilhermeMFranco@users.noreply.github.com>
2023-10-31 23:20:11 -0300

    Update calculadora.py
    Calculadora padrão em python.

--- calculadora.py
+++ calculadora.py
@@ -1,20 +1,32 @@
 import os
 
-primeiro_valor = float(input(f"Informe o primeiro valor para ser calculado: {os.linesep} [1] [2] [3] [/] {os.linesep} [4] [5] [6] [x] {os.linesep} [7] [8] [9] [-] {os.linesep} [#] [0] [*] [+]{os.linesep}"))
-operacao = input("Escolha a operação desejada (+, -, *, /):")
-segundo_valor = float(input("Informe o segundo valor: "))
+print("***************CALCULADORA PYTHON***************")
+primeiro_valor = int(input(f"Informe o primeiro valor da operação:{os.linesep}[1] [2] [3] [/]{os.linesep}[4] [5] [6] [*]{os.linesep}[4] [5] [6] [-]{os.linesep}[7] [8] [9] [+] {os.linesep}[*] [0] [#] [=]{os.linesep }"))
+operacao = input("Escolha um operação matemática: (+, -, *, /)")
+segundo_valor = int(input("Escolha o segundo valor da operação: "))
 
 if operacao == "+":
     resultado = primeiro_valor + segundo_valor
     print(f"Resultado: {resultado}")
-elif operacao == "*":
-    resultado = primeiro_valor * segundo_valor
-    print(f"Resultado: {resultado}")
-elif operacao == "-":
+if operacao == "-":
     resultado = primeiro_valor - segundo_valor
     print(f"Resultado: {resultado}")
-elif operacao == "/":
+if operacao == "*":
+    resultado = primeiro_valor * segundo_valor
+    print(f"Resultado: {resultado}")
+if operacao == "/":
     resultado = primeiro_valor / segundo_valor
     print(f"Resultado: {resultado}")
-else:
-    print("Operação inválida.")
+
+
+'''***************CALCULADORA PYTHON***************
+Informe o primeiro valor da operação:
+[1] [2] [3] [/]
+[4] [5] [6] [*]
+[4] [5] [6] [-]
+[7] [8] [9] [+]
+[*] [0] [#] [=]
+10
+Escolha um operação matemática: (+, -, *, /)*
+Escolha o segundo valor da operação: 10
+Resultado: 100'''
