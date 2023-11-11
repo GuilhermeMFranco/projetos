d615ea7df4b6788d9411a5e16282aacc5992618a
GuilhermeMFranco <148026827+GuilhermeMFranco@users.noreply.github.com>
2023-11-11 01:29:10 -0300

    Create convercao_numero_calcado.py
    Conversão para calçados medido em centímetros e convertendo para numero padrão de calçados.

---
+++ convercao_numero_calcado.py
@@ -0,0 +1,5 @@
+print("**********Tabelas de conversão de tamanhos de calçado**********")
+medida_pes = float(input("Informe a medida do seus pés em centímetros: "))
+conta = (medida_pes * 5 + 28) / 4
+print(f"Você calça numero: {conta}")
+
